35d59f4dfdd02165741fe8bd97e851a1ff9ae58f
Gabriel Santos <gabrielrsantoss@icloud.com>
2024-07-03 20:24:02 -0300

    Initial Commit

---
+++ VolleyScoreboard/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  VolleyScoreboard
+//
+//  Created by Gabriel Santos on 03/07/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
