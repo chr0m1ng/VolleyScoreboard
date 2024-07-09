d4d700e3fc108ae378f4c008f493e3f3a7ad1729
Gabriel Santos <gabrielrsantoss@icloud.com>
2024-07-09 16:09:16 -0300

    add: watchOS project

---
+++ VolleyScoreboardWatch Watch App/ScoreboardView.swift
@@ -0,0 +1,24 @@
+//
+//  ScoreboardView.swift
+//  VolleyScoreboardWatch Watch App
+//
+//  Created by Gabriel Santos on 09/07/24.
+//
+
+import SwiftUI
+
+struct ScoreboardView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ScoreboardView()
+}
