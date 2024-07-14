dedefe58900051d4edcb806981ef7db398653c43
Gabriel Santos <gabrielrsantoss@icloud.com>
2024-07-13 21:38:35 -0300

    add: date separator in game list

---
+++ VolleyScoreboard/Views/ListViews/DateSeparatorView.swift
@@ -0,0 +1,18 @@
+//
+//  DateSeparatorView.swift
+//  VolleyScoreboard
+//
+//  Created by Gabriel Santos on 13/07/24.
+//
+
+import SwiftUI
+
+struct DateSeparatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DateSeparatorView()
+}
